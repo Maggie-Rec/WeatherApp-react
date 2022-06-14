d264593e0744c2115b295aa4c19c108d280f219a
maggie-Rec <marzena.gromo@gmail.com>
2022-06-14 16:01:51 +0100

    added footer and link to repo in GitHub

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Maggie RG and is{" "}
+        <a href="https://github.com/Maggie-Rec/WeatherApp-react">
+          {" "}
+          open-sourced of GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
